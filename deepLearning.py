87a878f87d5a5df7f6e6eb57206d579e84359c13
sid-FBLA <sid20102004@gmail.com>
2021-01-22 00:27:51 -0500

    day1

--- deepLearning.py
+++ deepLearning.py
@@ -44,12 +44,30 @@ x_train = []
 #y_train is the value we want the model to predict
 y_train = []
 
+#set variable for length of training set
+initial = 70
+
 #starts from 70 --> length of dataset
 for i in range(70, training_data_len):
     #appends all data in x_train array, excluding 'i'
     x_train.append(train_data[i-70:i, 0])
+    #only gets i
     y_train.append(train_data[i, 0])
-    if i <= 70:
+    if i <= 71:
         print(i);
         print(x_train)
         print(y_train)
+
+
+#convert x_train and y_train to numpy arrays
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+print(np.mean(x_train))
+print(np.mean(y_train))
+
+#Reshape the data
+#LSTM Network expects 3D input
+#, # of samples (rows), # of time steps (columns), # of Features (close option), current set is 2D
+print(x_train.shape)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+print(x_train.shape)
